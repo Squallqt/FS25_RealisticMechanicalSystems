a2fc3e6346510d2a8b546dfefd8afaa3dee8f9eb
Maxime <squallqt@free.fr>
2026-07-08 18:29:47 +0200

    Fix ADS vehicle farm ownership validation

--- FS25_AdvancedDamageSystem/scripts/core/ADS_Specialization.lua
+++ FS25_AdvancedDamageSystem/scripts/core/ADS_Specialization.lua
@@ -2909,7 +2909,15 @@ end
 
 local function registerVehicle(vehicle)
     if ADS_Main and ADS_Main.vehicles and ADS_Main.vehicles[vehicle.uniqueId] == nil then
-        if (vehicle.propertyState == 2 or vehicle.propertyState == 3 or vehicle.propertyState == 4) and vehicle.ownerFarmId ~= 0 and vehicle.ownerFarmId < 10 then
+        local ownerFarmId = vehicle.getOwnerFarmId ~= nil and vehicle:getOwnerFarmId() or vehicle.ownerFarmId
+        local hasSupportedPropertyState = vehicle.propertyState == VehiclePropertyState.OWNED
+            or vehicle.propertyState == VehiclePropertyState.LEASED
+            or vehicle.propertyState == VehiclePropertyState.MISSION
+        local hasValidOwnerFarm = ownerFarmId ~= nil
+            and ownerFarmId ~= FarmManager.SPECTATOR_FARM_ID
+            and g_farmManager:getFarmById(ownerFarmId) ~= nil
+
+        if hasSupportedPropertyState and hasValidOwnerFarm then
 
             local spec = vehicle.spec_AdvancedDamageSystem
             if spec == nil then return end
